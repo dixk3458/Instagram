68b32c6680fc26d80a13d65ac662513e412e8e6e
Jaewoong <dixk3458@naver.com>
2023-12-25 19:07:48 +0900

    Sanity Studio 커스텀

---
+++ sanity-studio/schemas/post.js
@@ -0,0 +1,71 @@
+export default {
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    {
+      title: 'Author',
+      name: 'author',
+      type: 'reference',
+      to: [{type: 'user'}],
+    },
+    {
+      title: 'Photo',
+      name: 'photo',
+      type: 'image',
+    },
+    {
+      title: 'Likes',
+      name: 'likes',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [{type: 'user'}],
+        },
+      ],
+      validation: (Rule) => Rule.unique(),
+    },
+    {
+      title: 'Comments',
+      name: 'comments',
+      type: 'array',
+      of: [
+        {
+          title: 'Comment',
+          name: 'comment',
+          type: 'document',
+          fields: [
+            {
+              title: 'Author',
+              name: 'author',
+              type: 'reference',
+              to: [{type: 'user'}],
+            },
+            {
+              title: 'Comment',
+              name: 'comment',
+              type: 'string',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  preview: {
+    select: {
+      title: 'comments.0.comment',
+      authorName: 'author.name',
+      authorUserId: 'author.userid',
+      media: 'photo',
+    },
+    prepare(selection) {
+      const {title, authorName, authorUserId, media} = selection
+      return {
+        title,
+        subtitle: `by ${authorName} (${authorUserId})`,
+        media,
+      }
+    },
+  },
+}
